631777b46808eb24c70beb3a5c9ccd0ac51155c1
Cremona Lambert <cremonalambertmw@gmail.com>
2023-02-08 06:05:47 +0200

    semester one data

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 
-//   const firstAndSecondSemestercours = [
-//       {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
-//       {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
-//       {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
-//       {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
-//       {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
-//       {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
-//     ];
-// console.table(firstAndSecondSemestercours);
-
-const firstAndSecondSemestercours = {
-  firstYear:{
-          
-        };
-};
-.console.log(firstAndSecondSemestercours);
+  const firstAndSecondSemestercours = [
+      {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
+      {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
+      {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
+      {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
+      {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
+      {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
+    ];
+console.table(firstAndSecondSemestercours);
