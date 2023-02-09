0bfef3d4926840b8260134d8f9d399809fa0289c
Cremona Lambert <cremonalambertmw@gmail.com>
2023-02-09 03:36:45 +0200

    myAPI best format

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
 
-  const firstSemester01 = [
-      {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
-      {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
-      {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
-      {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
-      {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
-      {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
-    ];
+  // const firstSemester01 = [
+      // {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
+      // {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
+      // {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
+      // {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
+      // {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
+      // {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
+    // ];
 // console.table(firstSemester01);
 
-const secondSemester01 = [
+// const secondSemester01 = [
   // {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
   // {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
   // {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
-  {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
-  {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
-  {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
-];
+  // {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
+  // {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
+  // {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
+// ];
 
 const allModules = [
     {
+      id: 1,
       semester: "First Semester",
       courseCode: "EAP511S",
       courseTitle: "English for academic purposes",
       prerequisites: "English in practice, or Language in apractice B, or Module 3, or Exemption",
+      year: "first Year",
     },
 
     {
+      id: 2,
       semester: "First Semester",
       courseCode: "CEN510S",
       courseTitle: "Computing for Engineers 114",
       prerequisites: "none",
+      year: "first Year",
     },
 
     {
+      id: 3,
       semester: "First Semester",
       courseCode: "EAP511S",
       courseTitle: "Introduction to Engineering Drawing 115",
       prerequisites: "none",
+      year: "first Year",
     },
 
     {
+      id: 4,
       semester: "First Semester",
       courseCode: "EAP511S",
-      courseTitle: "Introduction to Engineering Drawing 115",
+      courseTitle: "Engineering Mathematics 115",
       prerequisites: "none",
+      year: "first Year",
     },
     
     {
+      id: 5,
       semester: "First Semester",
       courseCode: "EAP511S",
-      courseTitle: "Introduction to Engineering Drawing 115",
+      courseTitle: "Engineering Mechanics 114",
       prerequisites: "none",
+      year: "first Year",
     },
 
     {
+      id: 6,
       semester: "First Semester",
       courseCode: "EAP511S",
-      courseTitle: "Introduction to Engineering Drawing 115",
+      courseTitle: "Engineering Physics 114",
       prerequisites: "none",
+      year: "first Year",
     },
 
 ];
