7900fac778130a49cf2707a3c0e4034f7c0a39aa
CremonaLambert <cremonalambertmw@gmail.com>
2023-02-09 23:52:39 +0200

    course code updated

--- Nust_subjects/index.js
+++ Nust_subjects/index.js
@@ -1,23 +1,4 @@
 
-  // const firstSemester01 = [
-      // {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
-      // {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
-      // {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
-      // {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
-      // {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
-      // {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
-    // ];
-// console.table(firstSemester01);
-
-// const secondSemester01 = [
-  // {courseCode: 'EAP511S', courseTitle: 'English for academic purposes', prerequisites: 'English in practice, or Language in apractice B, or Module 3, or Exemption'},
-  // {courseCode: 'CEN510S', courseTitle: 'Computing for Engineers 114', prerequisites: 'none' },
-  // {courseCode: 'EAP511S', courseTitle: 'Introduction to Engineering Drawing 115', prerequisites: 'none' },
-  // {courseCode: 'EAP511S', courseTitle: 'Engineering Mathematics 115', prerequisites: 'none' },
-  // {courseCode: 'EAP511S', courseTitle: 'Engineering Mechanics 114', prerequisites: 'none' },
-  // {courseCode: 'EAP511S', courseTitle: 'Engineering Physics 114', prerequisites: 'none' },
-// ];
-
 const allModules = [
     {
       id: 1,
@@ -42,7 +23,7 @@ const allModules = [
     {
       id: 3,
       semester: "First Semester",
-      courseCode: "EAP511S",
+      courseCode: "IED511S",
       courseTitle: "Introduction to Engineering Drawing 115",
       prerequisites: "none",
       year: "first Year",
@@ -52,7 +33,7 @@ const allModules = [
     {
       id: 4,
       semester: "First Semester",
-      courseCode: "EAP511S",
+      courseCode: "MTM511S",
       courseTitle: "Engineering Mathematics 115",
       prerequisites: "none",
       year: "first Year",
@@ -62,7 +43,7 @@ const allModules = [
     {
       id: 5,
       semester: "First Semester",
-      courseCode: "EAP511S",
+      courseCode: "AMC511S",
       courseTitle: "Engineering Mechanics 114",
       prerequisites: "none",
       year: "first Year",
@@ -72,7 +53,7 @@ const allModules = [
     {
       id: 6,
       semester: "First Semester",
-      courseCode: "EAP511S",
+      courseCode: "APH511S",
       courseTitle: "Engineering Physics 114",
       prerequisites: "none",
       year: "first Year",
