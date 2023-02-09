ca5963f9886d6cad3c69f2eaf4b00d2130e1c621
CremonaLambert <cremonalambertmw@gmail.com>
2023-02-10 00:04:29 +0200

    semester 2

--- Nust_subjects/index.js
+++ Nust_subjects/index.js
@@ -2,63 +2,126 @@
 const allModules = [
     {
       id: 1,
-      semester: "First Semester",
+      semester: "Semester 1",
       courseCode: "EAP511S",
       courseTitle: "English for academic purposes",
       prerequisites: "English in practice, or Language in apractice B, or Module 3, or Exemption",
-      year: "first Year",
+      year: "First Year",
       departement: "mining",
     },
 
     {
       id: 2,
-      semester: "First Semester",
+      semester: "Semester 1",
       courseCode: "CEN510S",
       courseTitle: "Computing for Engineers 114",
       prerequisites: "none",
-      year: "first Year",
+      year: "First Year",
       departement: "mining",
     },
 
     {
       id: 3,
-      semester: "First Semester",
+      semester: "Semester 1",
       courseCode: "IED511S",
       courseTitle: "Introduction to Engineering Drawing 115",
       prerequisites: "none",
-      year: "first Year",
+      year: "First Year",
       departement: "mining",
     },
 
     {
       id: 4,
-      semester: "First Semester",
+      semester: "Semester 1",
       courseCode: "MTM511S",
       courseTitle: "Engineering Mathematics 115",
       prerequisites: "none",
-      year: "first Year",
+      year: "First Year",
       departement: "mining",
     },
     
     {
       id: 5,
-      semester: "First Semester",
+      semester: "Semester 1",
       courseCode: "AMC511S",
       courseTitle: "Engineering Mechanics 114",
       prerequisites: "none",
-      year: "first Year",
+      year: "First Year",
       departement: "mining",
     },
 
     {
       id: 6,
-      semester: "First Semester",
+      semester: "Semester 1",
       courseCode: "APH511S",
       courseTitle: "Engineering Physics 114",
       prerequisites: "none",
-      year: "first Year",
+      year: "First Year",
       departement: "mining",
     },
+
+    {
+      id: 7,
+      semester: "Semester 2",
+      courseCode: "",
+      courseTitle: "",
+      prerequisites: "none",
+      year: "First Year",
+      departement: "mining",
+    },
+
+    {
+      id: 8,
+      semester: "Semester 2",
+      courseCode: "",
+      courseTitle: "",
+      prerequisites: "none",
+      year: "First Year",
+      departement: "mining",
+    },
+
+    {
+      id: 9,
+      semester: "Semester 2",
+      courseCode: "",
+      courseTitle: "",
+      prerequisites: "none",
+      year: "First Year",
+      departement: "mining",
+    },
+
+    {
+      id: 10,
+      semester: "Semester 2",
+      courseCode: "IIN521S ",
+      courseTitle: "Introduction to Industry 4.0",
+      prerequisites: "none",
+      year: "First Year",
+      departement: "mining",
+    },
+
+    {
+      id: 11,
+      semester: "Semester 2",
+      courseCode: "",
+      courseTitle: "",
+      prerequisites: "none",
+      year: "First Year",
+      departement: "mining",
+    },
+
+    {
+      id: 12,
+      semester: "Semester 2",
+      courseCode: "",
+      courseTitle: "",
+      prerequisites: "none",
+      year: "First Year",
+      departement: "mining",
+    },
+
+
+
 
 ];
 console.log(allModules);
