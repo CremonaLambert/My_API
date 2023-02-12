774466cc9155feba96abcbb8162d73ab9a43eb3e
CremonaLambert <cremonalambertmw@gmail.com>
2023-02-12 08:58:34 +0200

    first year modules

---
+++ Nust_subjects/method2/nust.js
@@ -0,0 +1,140 @@
+year1 = {
+
+    semester1:{
+
+         module1:{
+            id: 1,
+            courseCode: "EAP511S",
+            courseTitle: "English for academic purposes",
+            prerequisites: "English in practice, or Language in apractice B, or Module 3, or Exemption",
+            NQFlevel: 5,
+            NQFcredits: 14,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module2:{
+            id: 2,
+            courseCode: "CEN510S",
+            courseTitle: "Computing for Engineers 114",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module3:{
+            id: 3,
+            courseCode: "IED511S",
+            courseTitle: "Introduction to Engineering Drawing 115",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 15,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module4:{
+            id: 4,
+            courseCode: "MTM511S",
+            courseTitle: "Engineering Mathematics 115",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 15,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module5:{
+            id: 5,
+            courseCode: "AMC511S",
+            courseTitle: "Engineering Mechanics 114",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module6:{
+            id: 6,
+            courseCode: "APH511S",
+            courseTitle: "Engineering Physics 114",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+    },
+
+    semester2:{
+        module1:{
+            id: 1,
+            courseCode: "IIN521S",
+            courseTitle: "Introduction to Industry 4.0",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module2:{
+            id: 2,
+            courseCode: "ELC520S",
+            courseTitle: "Electrical Circuits 124",
+            prerequisites: "Engineering Physics 114, Engineering Mathematics 115",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module3:{
+            id: 3,
+            courseCode: "ACH521S",
+            courseTitle: "Engineering Chemistry 123",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 9,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module4:{
+            id: 4,
+            courseCode: "MTM521S",
+            courseTitle: "Engineering Mathematics 125",
+            prerequisites: "Engineering Mathematics 115",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module5:{
+            id: 5,
+            courseCode: "MLS520S",
+            courseTitle: "Materials Science 124",
+            prerequisites: "None",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module6:{
+            id: 6,
+            courseCode: "MOM521S",
+            courseTitle: "Mechanics of Materials 124",
+            prerequisites: "Engineering Mechanics 114",
+            NQFlevel: 5,
+            NQFcredits: 12,
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+
+         module7:{
+            id: 7,
+            courseCode: "WPR421S",
+            courseTitle: "Workshop Practice 120",
+            prerequisites: "None",
+            NQFlevel: 4,
+            NQFcredits: "NCB",
+            departement: "MINING AND PROCESS ENGINEERING"
+         },
+    },
+};
+
+console.log(year1);
